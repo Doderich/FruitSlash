27c416976db4e234443a785ed358831a36836c89
Simon Müller <simuit01@hs-esslingen.de>
2023-04-24 16:30:24 +0200

    rip

---
+++ js/graphics/menu.mjs
@@ -0,0 +1,59 @@
+export let gotClicked = false;
+export function startButton() {
+  let props = {isDead: false, type: "startButton"};  
+  let canvas = document.getElementById("canvas");
+  let buttonWidth = 200;
+  let buttonHeight = 50;
+  let buttonX = canvas.width / 2 - buttonWidth / 2;
+  let buttonY = canvas.height / 2 - buttonHeight / 2;
+  let buttonText = "Start Game";
+  let textWidth = undefined;
+
+  function draw(ctx) {
+    if (!gotClicked) {
+      textWidth = ctx.measureText(buttonText).width;
+      ctx.fillStyle = "gray";
+      ctx.strokeStyle = "red";
+      ctx.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
+      ctx.strokeRect(buttonX, buttonY, buttonWidth, buttonHeight);
+      ctx.fillStyle = "black";
+      ctx.font = "20px Arial";
+      ctx.fillText(buttonText, buttonX + buttonWidth / 2 - textWidth / 2, buttonY + buttonHeight/ 2 + (20*0.8) /2);
+    }
+  }
+  function getClicked() {
+    return gotClicked;
+  }
+
+  function update() {}
+  function isTouched(ti, tx, ty) {
+    if (
+      tx >= buttonX &&
+      tx <= buttonX + buttonWidth &&
+      ty >= buttonY &&
+      ty <= buttonY + buttonHeight
+    ) {
+      gotClicked = true;
+    }
+  }
+  function reset() {}
+  function move() {}
+  function isDeleted() {
+    return false;
+  }
+  function getCoordinates() {
+    let dummy = { b: false };
+    return dummy;
+  }
+  return {
+    draw,
+    update,
+    isDeleted,
+    move,
+    isTouched,
+    reset,
+    getCoordinates,
+    getClicked,
+    props
+  };
+}
