cdcee21033909e759cb5094890bed2581a814744
Simon Müller <simuit01@hs-esslingen.de>
2023-04-25 01:17:48 +0200

    sheesh

--- js/graphics/menu.mjs
+++ js/graphics/menu.mjs
@@ -11,7 +11,6 @@ export function startButton() {
 
   function draw(ctx) {
     if (!props.gotClicked) {
-      ctx.save();
       textWidth = ctx.measureText(buttonText).width;
       ctx.fillStyle = "gray";
       ctx.strokeStyle = "red";
@@ -24,7 +23,6 @@ export function startButton() {
         buttonX + buttonWidth / 2 - textWidth / 2,
         buttonY + buttonHeight / 2 + (20 * 0.8) / 2
       );
-      ctx.restore();
     }
   }
 
@@ -35,6 +33,9 @@ export function startButton() {
   }
   function reset() {}
   function move() {}
+  function isDeleted() {
+    return false;
+  }
   function getCoordinates() {
     let dummy = { b: false };
     return dummy;
@@ -42,6 +43,7 @@ export function startButton() {
   return {
     draw,
     update,
+    isDeleted,
     move,
     isTouched,
     reset,
@@ -62,7 +64,6 @@ export function modeButton() {
   
   function draw(ctx) {
     if (!props.gotClicked) {
-      ctx.save();
     textWidth = ctx.measureText(buttonText).width;
     ctx.fillStyle = "gray";
     ctx.strokeStyle = "red";
@@ -75,7 +76,6 @@ export function modeButton() {
         buttonX + buttonWidth / 2 - textWidth / 2,
         buttonY + buttonHeight / 2 + (20 * 0.8) / 2
         );
-    ctx.restore();
      }
    }
     
@@ -120,57 +120,3 @@ function isTouched(touchindentifier, touchx, touchy) {
       props,
     };
 }
-
-/*export function timeButton() {
-  let props = { isDead: false, type: "timeButton", gotClicked: false };
-  let canvas = document.getElementById("canvas");
-  let buttonWidth = 200;
-  let buttonHeight = 50;
-  let buttonX = canvas.width / 2 - buttonWidth / 2;
-  let buttonY = canvas.height / 1.7 - buttonHeight / 2;
-  let buttonText = "Time";
-  let textWidth = undefined;
-      
-  function draw(ctx) {
-    if (!props.gotClicked) {
-    textWidth = ctx.measureText(buttonText).width;
-    ctx.fillStyle = "gray";
-    ctx.strokeStyle = "red";
-    ctx.fillRect(buttonX, buttonY, buttonWidth, buttonHeight);
-    ctx.strokeRect(buttonX, buttonY, buttonWidth, buttonHeight);
-    ctx.fillStyle = "black";
-    ctx.font = "20px Arial";
-    ctx.fillText(
-        buttonText,
-        buttonX + buttonWidth / 2 - textWidth / 2,
-        buttonY + buttonHeight / 2 + (20 * 0.8) / 2
-        );
-    }
-  }
-        
-    function update() {}
-    
-    function isTouched(touchindentifier, touchx, touchy) {
-        console.log("timeBtn is Touched");
-        props.gotClicked = true;
-    }
-    function reset() {}
-    function move() {}
-    function isDeleted() {
-        return false;
-    }
-    function getCoordinates() {
-        let dummy = { b: false };
-        return dummy;
-    }
-    return {
-        draw,
-        update,
-        isDeleted,
-        move,
-        isTouched,
-        reset,
-        getCoordinates,
-        props,
-    };
-} */
