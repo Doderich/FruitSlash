302592522e6fb0727ff8e71692a067bb20307e7a
Doderich <mabu@vollbio.de>
2023-04-25 04:17:00 +0200

    kind of working

--- js/gamelogic.mjs
+++ js/gamelogic.mjs
@@ -13,13 +13,16 @@ export function initLogic() {
   let { touchCallback, interactionsObjectsUpdate } =
     initInteraction(interactiveObjects);
   let i = 0;
+  let hp = 3;
   const Sword = swordObject.Sword();
   function draw(ctx, deltaTime) {
     checkGameState(deltaTime);
     interactionsObjectsUpdate(interactiveObjects); //
     if (i == 1) {
       for (let io of interactiveObjects) {
-        io.move();
+        if (io.move() && --hp <= 0) {
+          gameState = "lose";
+        }
         i = 0;
       }
     }
@@ -46,14 +49,14 @@ export function initLogic() {
     touchCallback((identifier, x, y) => {
       if (gameState == "ongoing") {
         let swordMatrix = Sword.drawSword(ctx, x, y);
-        let swordHitbox = Sword.swordHitbox(ctx, x, y, swordMatrix);
+        //let hitgoxmatrix = Sword.swordHitbox(ctx, x, y, swordMatrix);
         let sword = {
           x,
           y,
           width: 24,
           height: 115,
           rotation: 0,
-          matrix: swordHitbox,
+          matrix: swordMatrix,
         };
         if (
           checkCollisionWithInteractiveObject(sword, interactiveObjects, ctx)
@@ -61,14 +64,6 @@ export function initLogic() {
           console.warn("u toched a circle");
         }
       } else if (gameState == "menu") {
-        console.log(
-          interactiveObjects[0].props.type,
-          interactiveObjects[0].props.gotClicked
-        );
-        if (interactiveObjects[0].props.type == "startButton") {
-          gameState = "start";
-          console.log("in menu");
-        }
       }
     });
     i++;
@@ -77,43 +72,49 @@ export function initLogic() {
     if (gameState == "start") {
       interactiveObjects = [];
       interactiveObjects.push(
-        button(50, window.innerHeight - 80, window.innerWidth / 3, 50, () => {
-          gameState = "menu";
-        })
+        button(
+          50,
+          window.innerHeight - 80,
+          window.innerWidth / 3,
+          50,
+          () => {
+            gameState = "menu";
+          },
+          "reset"
+        )
       );
       interactiveObjects.push(Highscore());
       gameState = "ongoing";
     } else if (gameState == "menu") {
       interactiveObjects = [];
-      interactiveObjects.push(
-        button(
-          window.innerWidth / 2,
-          window.innerHeight / 4,
-          window.innerWidth / 4,
-          100,
-          "Endless",
-          () => {
-            gameState = "menu";
-          }
-        )
-      );
-      interactiveObjects.push(
-        button(
-          window.innerWidth / 2,
-          window.innerHeight / 4,
-          window.innerWidth / 4,
-          100,
-          "Start Game",
-          () => {
-            gameState = "start";
-          }
-        )
-      );
-    } else if (gameState == "ongoing") {
+      //interactiveObjects.push(
+      //  button(
+      //    window.innerWidth / 2 - 100,
+      //    window.innerHeight / 2 - 125,
+      //    200,
+      //    50,
+      //    () => {
+      //      gameState = "start";
+      //    },
+      //    "StartGame"
+      //  )
+      //);
+      //interactiveObjects.push(
+      //  button(
+      //    window.innerWidth / 2 - 100,
+      //    window.innerHeight / 2 - 25,
+      //    200,
+      //    50,
+      //    () => {
+      //      gameState = "menu";
+      //    },
+      //    "Endless"
+      //  )
+      //);
+      interactiveObjects.push(startButton());
     } else if (gameState == "win") {
     } else if (gameState == "lose") {
-      let highscore = interactiveObjects[1].props.score;
-      console.log("Highscore=> ", highscore);
+      //let highscore = interactiveObjects[1].props.score;
       interactiveObjects = [];
       interactiveObjects.push(
         button(
@@ -121,44 +122,16 @@ export function initLogic() {
           window.innerHeight - 80,
           window.innerWidth / 3,
           50,
-          "reset",
           () => {
             gameState = "menu";
-          }
+          },
+          "reset"
         )
       );
-      interactiveObjects.push(loseScreen(highscore));
+      interactiveObjects.push(LoseMsg());
     }
   }
   return { draw };
-}
-
-function spawnEnemies(interactionsObjects, width, height) {
-  const circles = [];
-  const circleRadius = 50;
-  const numCircles = 20;
-  for (let i = 0; i < numCircles; i++) {
-    let validLocation = false;
-    let x, y;
-    while (!validLocation) {
-      x = Math.floor(Math.random() * (width - 2 * circleRadius)) + circleRadius;
-      y =
-        Math.floor(Math.random() * (height - 2 * circleRadius - 200)) +
-        circleRadius;
-      validLocation = true;
-      for (let j = 0; j < i; j++) {
-        const dx = x - circles[j][0];
-        const dy = y - circles[j][1];
-        const distance = Math.sqrt(dx ** 2 + dy ** 2);
-        if (distance < 2 * circleRadius) {
-          validLocation = false;
-          break;
-        }
-      }
-    }
-    circles.push([x, y]);
-    interactionsObjects.push(Enemy(x, y, i));
-  }
 }
 function spawnEnemy(interactiveObjects, width, height, id) {
   let enemys = [];
@@ -222,6 +195,24 @@ function Highscore() {
     ctx.save();
     ctx.font = "50px Arial";
     ctx.fillText(props.score.toString(), window.innerWidth / 2 - 50, 100);
+    ctx.restore();
+  }
+  function reset() {}
+  function isTouched() {}
+  function move() {}
+  return { move, isTouched, reset, draw, props };
+}
+
+function LoseMsg() {
+  let props = { isDead: false, type: "text", score: 0 };
+  function draw(ctx) {
+    ctx.save();
+    ctx.font = "50px Arial";
+    ctx.fillText(
+      "You lost",
+      window.innerWidth / 2 - 50,
+      window.innerHeight / 2
+    );
     ctx.restore();
   }
   function reset() {}
